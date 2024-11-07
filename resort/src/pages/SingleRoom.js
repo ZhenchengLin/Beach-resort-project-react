798b71827872701401328ed7c7d841ffc7312e1f
zhenchenglin <zhl132@ucsd.edu>
2024-11-07 12:42:14 -0800

    init commit with pages and components

---
+++ resort/src/pages/SingleRoom.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class SignleRoom extends Component {
+  render() {
+    return (
+      <div>Hello from singleroom page</div>
+    )
+  }
+}
+
